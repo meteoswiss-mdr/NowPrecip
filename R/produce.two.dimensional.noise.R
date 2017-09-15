358604c0e33799453a9fc1ba75d5721faaee16a8
ned <daniele.nerini@meteoswiss.ch>
2017-09-15 15:43:36 +0200

    added new nested noise generator and update the global implementation

--- R/produce.two.dimensional.noise.R
+++ R/produce.two.dimensional.noise.R
@@ -1,9 +1,8 @@
 produce.two.dimensional.noise <-
    function( z
-			,size = 1024
 			,nr.frames = 60
-			,dim.x = 710
-			,dim.y = 640
+            ,win.type = 'flat.hanning'
+            ,do.set.seed = FALSE
 			,on.screen = FALSE
 			) {
 	#Produces a 2-dimensional correlated noise
@@ -11,24 +10,45 @@ produce.two.dimensional.noise <-
 	#Examples:
 	#z=Xln0[,,nr.obs]
 	#At = produce.two.dimensional.noise(z=Xln0[,,nr.obs])
+    
+    # make sure non-rainy pixels are set to zero
+    min.value = min(z)
+    orig.z = z
+    z = z - min.value
+    
+    # store original field size
+    orig.dim.x = dim(z)[1]
+    orig.dim.y = dim(z)[2]
+    orig.dm.size = c(orig.dim.x,orig.dim.y)
+    
+    # buffer the field with zeros to obtain a squared domain
+    dim.x = max(orig.dm.size) 
+    dim.y = dim.x
+    dm.size = c(dim.x,dim.y)
+    zs = array(0,dim=dm.size)
+    if(orig.dm.size[1] == dim.x){
+        idx.buffer = round((dim.y - orig.dim.y)/2)
+        zs[,idx.buffer:(idx.buffer+orig.dim.y-1)] = z
+    }else{
+        idx.buffer = round((dim.x - orig.dim.x)/2)
+        zs[idx.buffer:(idx.buffer+orig.dim.x-1),] = z
+    }
 
 	#Use the last observation as filter
-	dm.size = 512	
-	zs = z[(floor((dim.x-dm.size)/2)+1):(floor((dim.x-dm.size)/2)+dm.size),(floor((dim.y-dm.size)/2)+1):((floor(dim.y-dm.size)/2)+dm.size)]	
-	mfilter = fft(zs)
+    if(win.type == 'none'){
+        mask = array(1,dim=dm.size)
+    }else{
+        mask = build.two.dimensional.window(wsize=dm.size,wtype=win.type)
+    }
+	mfilter = fft(zs*mask)
 	mfilter = Mod(mfilter)
-	tmp = array(0,dim=c(1024,1024))
-	tmp[1:256,1:256] = mfilter[1:256,1:256]
-	tmp[(1024-256):1024,1:256] = mfilter[256:512,1:256]
-	tmp[1:256,(1024-256):1024] = mfilter[1:256,256:512]
-	tmp[(1024-256):1024,(1024-256):1024] = mfilter[256:512,256:512]
-	mfilter = tmp
-	
+    	
 	#Produce normal noise array
-	Ztf = array(rnorm(size^2*nr.frames) ,dim=c(size ,size ,nr.frames))
+    if(do.set.seed==TRUE) set.seed(42)
+	Ztf = array(rnorm(dm.size[1]*dm.size[2]*nr.frames) ,dim=c(dm.size[1],dm.size[2],nr.frames))
 	
 	#Power-filter images
-	result = array(dim=c(710,640,nr.frames))
+	result = array(dim=c(orig.dim.x,orig.dim.y,nr.frames))
 	for(m in 1:nr.frames) {
 		z.ftp = fft(Ztf[,,m])
 
@@ -38,15 +58,15 @@ produce.two.dimensional.noise <-
 		#If everything is correct all imaginary parts should be 0
 		z.cor = Re(z.iftp)
 		
-		#Crop a square equal to 710x640
+		#Crop a square equal to the original size
 		#If smaller than that just stick the z.cor at the corner of a
-		a = array(dim=c(dim.x,dim.y))
-		if(dim.y > dim(z.cor)[[1]]) {
+		a = array(dim=c(orig.dim.x,orig.dim.y))
+		if(orig.dim.y > dim(z.cor)[[1]]) {
 			a[1:size,1:size] = z.cor
 		} else {
-			difx = size-dim.x
-			dify = size-dim.y
-			a = z.cor[(difx/2+1):(dim.x+difx/2),(dify/2+1):(dim.y+dify/2)]	
+			difx = dim.x-orig.dim.x
+			dify = dim.y-orig.dim.y
+			a = z.cor[(difx/2+1):(dim.x-difx/2),(dify/2+1):(dim.y-dify/2)]	
 		}
 		
 		#Bring everything back to a N(0,1)
@@ -61,4 +81,90 @@ produce.two.dimensional.noise <-
 	
 	
 	return(result)
+}
+
+build.two.dimensional.window <-
+    function( wsize, wtype = 'flat.hanning'
+            ) {
+             
+    two.dimensional.window <- tryCatch(
+        {
+        # Asymmetric window
+        
+        # dim 1
+        switch(wtype,
+        
+            hanning={
+                w1d1 = 0.5 - 0.5*cos(2*pi*seq(wsize[1])/(wsize[1] - 1))
+            },
+            
+            flat.hanning={
+                T = wsize[1]/4
+                W = wsize[1]/2
+                B = seq(-W,W,length.out=2*W)
+                R = abs(B)-T
+                R[R<0]=0.
+                A = 0.5*(1.0 + cos(pi*R/T))
+                A[abs(B)>(2*T)]=0.0
+                w1d1 = A
+            },
+            
+            stop('Unsupported window type.')
+        )
+        
+        # dim 2
+        switch(wtype,
+        
+            hanning={
+                w1d2 = 0.5 - 0.5*cos(2*pi*seq(wsize[2])/(wsize[2] - 1))
+            },
+            
+            flat.hanning={
+                T = wsize[2]/4
+                W = wsize[2]/2
+                B = seq(-W,W,length.out=2*W)
+                R = abs(B)-T
+                R[R<0]=0.
+                A = 0.5*(1.0 + cos(pi*R/T))
+                A[abs(B)>(2*T)]=0.0
+                w1d2 = A
+            },
+            
+            stop('Unsupported window type.')
+        )
+        
+        # 2d window
+        two.dimensional.window = sqrt(outer(w1d1,w1d2))
+        
+        },
+        error=function(cond) {
+        
+        # Symmetric window
+        switch(wtype,
+        
+            hanning={
+                w1d = 0.5 - 0.5*cos(2*pi*seq(wsize[1])/(wsize[1] - 1))
+            },
+            
+            flat.hanning={
+                T = wsize[1]/4
+                W = wsize[1]/2
+                B = seq(-W,W,length.out=2*W)
+                R = abs(B)-T
+                R[R<0]=0.
+                A = 0.5*(1.0 + cos(pi*R/T))
+                A[abs(B)>(2*T)]=0.0
+                w1d = A
+            },
+            
+            stop('Unknown window type.')
+        )
+        # 2d window
+        two.dimensional.window = sqrt(outer(w1d,w1d))
+
+        return(two.dimensional.window)
+        }
+    )  
+    
+    return(two.dimensional.window)
 }
---
+++ R/produce.two.dimensional.noise.local.R
@@ -0,0 +1,341 @@
+produce.two.dimensional.noise.local <-
+    function( z
+			,nr.frames = 60
+            ,max.level = 1              # 0: global noise, >0: various degrees of localization
+            ,win.type  = 'flat.hanning' # 'hanning' or 'flat.hanning'
+            ,war.thr   = .1             
+            ,overlap   = 10
+            ,do.set.seed = FALSE
+			,on.screen = FALSE
+			) {
+            
+	#Produces a 2-dimensional correlated noise
+	#Use the last observation as filter
+    #Nested implementation to account for non-stationarities
+	#Example:
+	#z=Xln0[,,nr.obs]
+	#At = produce.two.dimensional.noise.local(z=Xln0[,,nr.obs])
+    #Created:
+    #ned, September 2017
+    
+    ## Input image
+    
+    # make sure non-rainy pixels are set to zero
+    min.value = min(z)
+    orig.z = z
+    z = z - min.value
+    
+    # store original field size
+    orig.dim.x = dim(z)[1]
+    orig.dim.y = dim(z)[2]
+    orig.dm.size = c(orig.dim.x,orig.dim.y)
+    
+    # buffer the field with zeros to obtain a squared domain
+    dim.x = max(orig.dm.size) 
+    dim.y = dim.x
+    dm.size = c(dim.x,dim.y)
+    ztmp = array(0,dim=dm.size)
+    if(orig.dm.size[1] == dim.x){
+        idx.buffer = round((dim.y - orig.dim.y)/2)
+        ztmp[,idx.buffer:(idx.buffer+orig.dim.y-1)] = z
+    }else{
+        idx.buffer = round((dim.x - orig.dim.x)/2)
+        ztmp[idx.buffer:(idx.buffer+orig.dim.x-1),] = z
+    }
+    z=ztmp
+    
+    ## Nested algorithm
+       
+    # prepare indices
+    Idxi = matrix(c(1,dim.x),nrow=1) 
+    Idxj = matrix(c(1,dim.y),nrow=1) 
+    Idxipsd = matrix(c(1,2^max.level),nrow=1) 
+    Idxjpsd = matrix(c(1,2^max.level),nrow=1) 
+    
+    # generate the FFT sample frequencies
+    res.km = 1 
+    freq = get.fftfreq(dim.x,res.km)
+    fx = outer(freq*0, freq, FUN = '+')
+    fy = outer(freq, freq*0, FUN = '+')
+    freq.grid = sqrt(fx^2 + fy^2)
+    
+    # get global fourier filter
+    this.window=build.two.dimensional.window(wsize=dm.size,wtype=win.type)
+    mfilter0 = get.fourier.filter(z*this.window)
+    # and allocate it to the final grid
+    mfilter = array(0, dim=c(2^max.level,2^max.level,dim(mfilter0)[1],dim(mfilter0)[2]))
+    mfilter = sweep(mfilter,c(3,4),mfilter0,'+')
+
+    # now loop levels and build composite spectra
+    level=0
+    while(level < max.level){
+
+        for(m in 1:dim(Idxi)[1]){
+            # cat('---------\n')
+            # print(c(level,m))
+            # print(c(Idxi[m,],Idxj[m,]))
+            # cat('------\n')
+            
+            # the indices of rainfall field
+            Idxnext = split.field(Idxi[m,],Idxj[m,],2)
+            Idxinext = Idxnext[[1]]; Idxjnext = Idxnext[[2]]
+            # the indices of the field of fourier filters
+            Idxpsdnext = split.field(Idxipsd[m,],Idxjpsd[m,],2)
+            Idxipsdnext = Idxpsdnext[[1]]; Idxjpsdnext = Idxpsdnext[[2]]
+            
+            for(n in 1:dim(Idxinext)[1]){
+                # print(c(n,Idxinext[n,],Idxjnext[n,]))
+
+                mask = get.mask(dm.size[1],Idxinext[n,],Idxjnext[n,])
+                war = sum((z*mask)>0)/(Idxinext[n,2]-Idxinext[n,1]+1)^2 
+                if(war>war.thr){
+                    # the new filter 
+                    newfilter = get.fourier.filter(z*mask)
+                    
+                    # compute logistic function to define weights as function of frequency
+                    # k controls the shape of the weighting function
+                    merge.weights = logistic.function(1/freq.grid, k=0.01, x0 = (Idxinext[n,2] - Idxinext[n,1] + 1)/2)
+                    newfilter = newfilter*(1-merge.weights)
+                    
+                    # perform the weighted average of previous and new fourier filters
+                    if(length(dim(mfilter[Idxipsdnext[n,1]:Idxipsdnext[n,2],Idxjpsdnext[n,1]:Idxjpsdnext[n,2],,]))>2){
+                        marg=c(3,4)
+                    }else{
+                        marg=c(1,2)
+                    }
+                    
+                    mfilter[Idxipsdnext[n,1]:Idxipsdnext[n,2],Idxjpsdnext[n,1]:Idxjpsdnext[n,2],,]=sweep(mfilter[Idxipsdnext[n,1]:Idxipsdnext[n,2],Idxjpsdnext[n,1]:Idxjpsdnext[n,2],,],marg,merge.weights,'*')
+                    
+                    mfilter[Idxipsdnext[n,1]:Idxipsdnext[n,2],Idxjpsdnext[n,1]:Idxjpsdnext[n,2],,]=sweep(mfilter[Idxipsdnext[n,1]:Idxipsdnext[n,2],Idxjpsdnext[n,1]:Idxjpsdnext[n,2],,],marg,newfilter,'+')
+                }
+            }
+        }
+        
+        # update indices
+        level = level + 1
+        Idx = split.field(c(1,dm.size[1]),c(1,dm.size[2]),2^level)
+        Idxi = Idx[[1]]; Idxj = Idx[[2]]
+        Idxpsd = split.field(c(1,2^max.level),c(1,2^max.level),2^level)
+        Idxipsd  = Idxpsd [[1]]; Idxjpsd  = Idxpsd[[2]]
+
+    }
+    
+    ## Power-filter images
+
+	# produce normal noise array
+    if(do.set.seed==TRUE) set.seed(42)
+	white.noise = array(rnorm(dm.size[1]*dm.size[2]*nr.frames) 
+                ,dim=c(dm.size[1] ,dm.size[2] ,nr.frames))
+    
+    # build composite image of correlated noise
+    corr.noise = array(0,dim=c(dim.x,dim.y,nr.frames))
+    sum.of.masks = array(0,dim=c(dim.x,dim.y,nr.frames))
+    idxi = array(0,dim=c(2,1))
+    idxj = array(0,dim=c(2,1))
+    winsize = ceiling( ( dm.size[1] + (2^max.level-1)*overlap ) / 2^max.level )
+    
+    # loop frames
+	for(m in 1:nr.frames) {
+        # loop rows
+        for(i in 0:(2^max.level-1)){
+            # loop columns
+            for(j in 0:(2^max.level-1)){
+            
+            # apply fourier filtering with local filter
+            this.filter = mfilter[i+1,j+1,,]
+            z.ftp = fft(white.noise[,,m])
+            z.ftp.fl = z.ftp * this.filter
+            z.iftp = fft(z.ftp.fl ,inverse=T)
+            z.cor = Re(z.iftp)
+            
+            # compute indices of local area
+            idxi[1] = i*winsize - i*overlap + 1
+            idxi[2] = min( c(idxi[1] + winsize - 1, dm.size[1]) )
+            idxj[1] = j*winsize - j*overlap + 1
+            idxj[2] = min( c(idxj[1] + winsize - 1, dm.size[2]) )
+            
+            # build mask and add local noise field to the composite image
+            mask = get.mask(dm.size[1],idxi,idxj)
+            corr.noise[,,m] = corr.noise[,,m] + z.cor*mask
+            sum.of.masks[,,m] = sum.of.masks[,,m] + mask
+            }
+        }
+	}	
+    
+    # normalize the sum
+    idx = sum.of.masks > 0
+    corr.noise[idx] = corr.noise[idx]/sum.of.masks[idx]
+    
+    # crop the image back to the original size
+    difx = dim.x-orig.dim.x
+    dify = dim.y-orig.dim.y
+    output = corr.noise[(difx/2+1):(dim.x-difx/2),(dify/2+1):(dim.y-dify/2),]
+    if(nr.frames==1) dim(output) = c(dim(output),1)
+
+    # standardize the results to N(0,1)
+    for(m in 1:nr.frames) {
+        output[,,m]  = ( output[,,m] - mean(output[,,m]) ) / sd(output[,,m])
+    }
+    
+    # plot results
+    if(on.screen==TRUE) {
+        x11()
+        image(output[,,1], breaks=seq(-5,5,0.1), col = rainbow(100))
+	}
+	
+	return(output)
+}
+
+build.two.dimensional.window <-
+    function( wsize, wtype = 'flat.hanning'
+            ) {
+             
+    two.dimensional.window <- tryCatch(
+        {
+        # Asymmetric window
+        
+        # dim 1
+        switch(wtype,
+        
+            hanning={
+                w1d1 = 0.5 - 0.5*cos(2*pi*seq(wsize[1])/(wsize[1] - 1))
+            },
+            
+            flat.hanning={
+                T = wsize[1]/4
+                W = wsize[1]/2
+                B = seq(-W,W,length.out=2*W)
+                R = abs(B)-T
+                R[R<0]=0.
+                A = 0.5*(1.0 + cos(pi*R/T))
+                A[abs(B)>(2*T)]=0.0
+                w1d1 = A
+            },
+            
+            stop('Unknown window type.')
+        )
+        
+        # dim 2
+        switch(wtype,
+        
+            hanning={
+                w1d2 = 0.5 - 0.5*cos(2*pi*seq(wsize[2])/(wsize[2] - 1))
+            },
+            
+            flat.hanning={
+                T = wsize[2]/4
+                W = wsize[2]/2
+                B = seq(-W,W,length.out=2*W)
+                R = abs(B)-T
+                R[R<0]=0.
+                A = 0.5*(1.0 + cos(pi*R/T))
+                A[abs(B)>(2*T)]=0.0
+                w1d2 = A
+            },
+            
+            stop('Unknown window type.')
+        )
+        
+        # 2d window
+        two.dimensional.window = sqrt(outer(w1d1,w1d2))
+        
+        },
+        error=function(cond) {
+        
+        # Symmetric window
+        switch(wtype,
+        
+            hanning={
+                w1d = 0.5 - 0.5*cos(2*pi*seq(wsize[1])/(wsize[1] - 1))
+            },
+            
+            flat.hanning={
+                T = wsize[1]/4
+                W = wsize[1]/2
+                B = seq(-W,W,length.out=2*W)
+                R = abs(B)-T
+                R[R<0]=0.
+                A = 0.5*(1.0 + cos(pi*R/T))
+                A[abs(B)>(2*T)]=0.0
+                w1d = A
+            },
+            
+            stop('Unknown window type.')
+        )
+        # 2d window
+        two.dimensional.window = sqrt(outer(w1d,w1d))
+
+        return(two.dimensional.window)
+        }
+    )  
+    
+    return(two.dimensional.window)
+}
+
+get.fourier.filter <-
+    function( fieldin, do.norm = TRUE){
+            
+    # FFT of the field
+    mfilter = fft(fieldin)
+    
+    # Normalize the real and imaginary parts
+    if(do.norm==TRUE) {
+        real.part = ( Re(mfilter) - mean(Re(mfilter)) ) / sd(Re(mfilter)) 
+        imag.part = ( Im(mfilter) - mean(Im(mfilter)) ) / sd(Im(mfilter))
+        mfilter = complex(real = real.part, imaginary = imag.part)
+        mfilter = matrix(mfilter,nrow=dim(fieldin)[1],byrow=F)
+    }
+    
+    # Extract the magnitude
+	mfilter = Mod(mfilter)    
+            
+    return(mfilter)        
+}
+    
+get.fftfreq <-
+    function(n, d=1.0){
+    
+    if(n %% 2 == 0){
+        f = c(0:(n/2-1), (-n/2):-1)/(d*n)
+    }else{
+        f = c(0:((n-1)/2), (-(n-1)/2):-1)/(d*n)
+    }
+    return(f)
+}
+
+split.field <-
+    function(idxi,idxj,Segments){
+    sizei = (idxi[2] - idxi[1]) + 1
+    sizej = (idxj[2] - idxj[1]) + 1
+    winsizei = round( sizei / Segments )
+    winsizej = round( sizej / Segments )
+    Idxi = array(0,dim=c(Segments^2,2))
+    Idxj = array(0,dim=c(Segments^2,2))
+    count=0
+    for(i in seq(0,Segments-1)){
+        for(j in seq(0,Segments-1)){
+            count=count+1
+            Idxi[count,1] = idxi[1] + i*winsizei
+            Idxi[count,2] = min( c(Idxi[count,1] + winsizei - 1, idxi[2]) )
+            Idxj[count,1] = idxj[1] + j*winsizej
+            Idxj[count,2] = min( c(Idxj[count,1] + winsizej - 1, idxj[2]) )
+        }
+    }
+    return(list(Idxi,Idxj))
+}
+
+get.mask <-
+    function(Size,idxi,idxj){
+    winsize = c(idxi[2] - idxi[1] + 1, idxj[2] - idxj[1] + 1)
+    wind = build.two.dimensional.window(winsize)
+    mask = array(0,dim=c(Size,Size)) 
+    mask[idxi[1]:idxi[2],idxj[1]:idxj[2]] = wind
+    return(mask)
+}
+
+logistic.function <-
+    function(x,L = 1,k = 1,x0 = 0){
+    return(L/(1 + exp(-k*(x - x0))))
+}
+
+
